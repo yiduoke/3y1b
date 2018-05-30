260cc3f348f5c0ad14b516762828e6613a3f1472
MdAbedin <mabedin1@stuy.edu>
2018-05-30 17:16:04 -0400

    task db functions

--- organize/utils/dbHelper.py
+++ organize/utils/dbHelper.py
@@ -1,20 +1,32 @@
+'''
+todo:
+- make stuff not crash when duplicate tasks are entered
+- add timeDifference to tasks table
+'''
+
 import sqlite3
+from datetime import datetime
 
 '''
 IMPORTANT NOTES:
--    to enter strings into the db, you have to pass in stuff like '"text"' instead of just 'text'
+-    if you make your own db modify functions, make sure to enter strings as '"text"' instead of just 'text'
+-    the functions i've written work with normal strings as arguments though, so don't pass '"text"' into them
 -    always save and close db at the end if you add any new db modification functions
 '''
 
 dbPath = 'data/db.db'
 
+def switchDb(path):
+    global dbPath
+    dbPath = path
+
 def openDb():
-    db = sqlite3.connect(dbPath)
+    db = sqlite3.connect(dbPath, detect_types = sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
     return db
 
 def getCursor(db):
-    cursor = db.cursor()
-    return cursor
+    c = db.cursor()
+    return c
 
 def saveDb(db):
     db.commit()
@@ -73,32 +85,66 @@ def insertRow(tableName, columns, values):
 def validateLogin(username, password):
     db = openDb()
     cursor = getCursor(db)
-    cmdString = 'SELECT * FROM users WHERE username = "%s" AND password = "%s";' % (username, password)
-    accounts = cursor.execute(cmdString).fetchone()
+    cmdString = 'SELECT password FROM users WHERE username = "%s";' % (username,)
+    passwords = cursor.execute(cmdString).fetchone()
     
     closeDb(db)
     #print passwords
     
-    return accounts != None
+    if passwords == None:
+        return False
+        
+    return passwords[0] == password
     
 
 #returns True if a username is registered, False otherwise
 def userExists(username):
     db = openDb()
     cursor = getCursor(db)
-    cmdString = 'SELECT * FROM users WHERE username = "%s";' % (username,)
-    accounts = cursor.execute(cmdString).fetchone()
+    cmdString = 'SELECT username FROM users WHERE username = "%s";' % (username,)
+    usernames = cursor.execute(cmdString).fetchone()
     #print usernames
     
     closeDb(db)
     
-    return accounts != None
+    return usernames != None
 
-#adds a new user
+#adds a new user. adds their login info to users table, and makes a new unique table for their tasks
 def addUser(username, password):
+    createTable(username, [['task', 'TEXT PRIMARY KEY'], ['startTime', 'TIMESTAMP'], ['endTime', 'TIMESTAMP'], ['expectedTime', 'INTEGER'], ['actualTime', 'REAL']])
+    
     db = openDb()
     cursor = getCursor(db)
-    cmdString = "INSERT INTO users (username, password) VALUES ('%s', '%s');" % (username, password)
-    cursor.execute(cmdString)
+    insertUser = "INSERT INTO users (username, password) VALUES ('%s', '%s');" % (username, password)
+    cursor.execute(insertUser)
+    
+    saveDb(db)
+    closeDb(db)
+
+#creates the table of usernames and passwords
+def createUsersTable():
+    createTable('users', [['username', 'TEXT PRIMARY KEY'], ['password', 'TEXT']])
+    
+#adds a task to a specific user's task table. the endTime and actualTime columns are set to -1 because they're determined on task completion, not creation
+def addTask(username, task, startTime, expectedTime):
+    db = openDb()
+    cursor = getCursor(db)
+    
+    cursor.execute('INSERT INTO ' + username + ' VALUES (?, ?, ?, ?, ?)', (task, startTime, '-1', expectedTime, '-1'))
+
+    saveDb(db)
+    closeDb(db)
+
+def completeTask(username, task):
+    db = openDb()
+    cursor = getCursor(db)
+    
+    currentTime = datetime.now()
+    cursor.execute('SELECT startTime FROM %s WHERE task = ?' % (username), (task,))
+    actualTime = currentTime - cursor.fetchone()[0]
+    actualTime = actualTime.seconds/60.0
+    
+    cursor.execute('UPDATE ' + username + ' SET endTime = ?, actualTime = ? WHERE task = ?', (datetime.now(), actualTime, task))
+
     saveDb(db)
     closeDb(db)
